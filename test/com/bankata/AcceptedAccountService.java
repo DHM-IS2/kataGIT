18871da3052b0006bbc36d138a8385030cc94eae
Osvaldo <ofarinas12@gmail.com>
2015-03-11 12:56:31 +0100

    First  test created

--- test/com/bankata/AcceptedAccountService.java
+++ test/com/bankata/AcceptedAccountService.java
@@ -1,7 +1,13 @@
 package com.bankata;
 
+import org.junit.Test;
+
 /**
  * Created by Osvaldo on 11/03/2015.
  */
 public class AcceptedAccountService {
+    @Test
+    public void should_test() throws Exception {
+    
+    }
 }
