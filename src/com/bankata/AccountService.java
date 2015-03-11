6ef08a9606067265503ea51237da5c3831568f30
Osvaldo <ofarinas12@gmail.com>
2015-03-11 13:27:39 +0100

    Injecting clock to account service

--- src/com/bankata/AccountService.java
+++ src/com/bankata/AccountService.java
@@ -1,6 +1,11 @@
 package com.bankata;
 
 public class AccountService {
+    private Clock clock;
+
+    public AccountService(Clock clock) {
+        this.clock = clock;
+    }
 
     public String haciendoEsto(){
         return "esto";
