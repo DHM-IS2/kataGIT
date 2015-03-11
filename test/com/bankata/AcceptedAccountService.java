56d55105a9d3613f29640e3080f0c672cd9ba945
Osvaldo <ofarinas12@gmail.com>
2015-03-11 13:12:36 +0100

    Print hi

--- test/com/bankata/AcceptedAccountService.java
+++ test/com/bankata/AcceptedAccountService.java
@@ -1,8 +1,10 @@
 package com.bankata;
 
+import org.junit.Test;
+
 public class AcceptedAccountService {
     @Test
     public void should_test() throws Exception {
-    
+        System.out.println("hola...");
     }
 }
